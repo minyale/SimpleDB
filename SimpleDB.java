7dc6fc4e3a5d26a3f04be7a77e21bb50a7ec8ba4
Mandy Ye <minyalecn@gmail.com>
2016-12-16 21:02:23 -0600

    Add files via upload

--- SimpleDB.java
+++ SimpleDB.java
@@ -1,168 +1,163 @@
 import java.util.*;
 
-
 //import SimpleDB.Command;
 public class SimpleDB {
-	//name_value map
+	// name_value map
 	private HashMap<String, Integer> nameValue;
-	//value_count map
+	// value_count map
 	private HashMap<Integer, Integer> valueCount;
-	//ROLLBACK Stack
+	// ROLLBACK Stack
 	private Deque<String> rollbackStack;
-	//# of Transaction blocks (begin)
+	// # of Transaction blocks (begin)
 	private int numBlocks;
-	
-	public SimpleDB(){
-		nameValue = new HashMap<String,Integer>();
-		valueCount = new HashMap<Integer,Integer>();
+
+	public SimpleDB() {
+		nameValue = new HashMap<String, Integer>();
+		valueCount = new HashMap<Integer, Integer>();
 		rollbackStack = new ArrayDeque<String>();
 		numBlocks = 0;
 	}
-	
-	
+
 	/**
-	 * Handle command line
-	 * To make it simple, assume all input format are valid here
-	 */ 	
+	 * Handle command line To make it simple, assume all input format are valid
+	 * here
+	 */
 	public void handleCommandLine(String line) {
-		String[] tokens = line.split("\\s+");	
+		String[] tokens = line.split("\\s+");
 		String cmd = tokens[0];
 		String name;
 		int value;
 		boolean isTransaction;
 		switch (cmd.toUpperCase()) {
-			case "SET":				
-				name = tokens[1];
-				value = Integer.parseInt(tokens[2]);
-				isTransaction = numBlocks == 0? false: true;
-				set(name, value, isTransaction);				
-				break;
-			
-			case "GET":				
-				name = tokens[1];
-				get(name);
-				break;
-			
-			case "UNSET":
-				name = tokens[1];
-				isTransaction = numBlocks == 0? false: true;
-				unset(name,isTransaction);				
-				break;
-			
-			case "NUMEQUALTO":
-				value = Integer.parseInt(tokens[1]);
-				numEqualTo(value);
-				break;
-			
-			case "BEGIN":				
-				begin();
-				break;
-			
-			case "ROLLBACK":
-				rollback();				
-				break;
-			
-			case "COMMIT":			
-				commit();
-				break;
-			
-			case "END":
-				System.exit(0);
-			
-			default:
-				System.err.println("Invalid input: " + line + "\n");
-				break;	
-			
+		case "SET":
+			name = tokens[1];
+			value = Integer.parseInt(tokens[2]);
+			isTransaction = numBlocks == 0 ? false : true;
+			set(name, value, isTransaction);
+			break;
+
+		case "GET":
+			name = tokens[1];
+			get(name);
+			break;
+
+		case "UNSET":
+			name = tokens[1];
+			isTransaction = numBlocks == 0 ? false : true;
+			unset(name, isTransaction);
+			break;
+
+		case "NUMEQUALTO":
+			value = Integer.parseInt(tokens[1]);
+			numEqualTo(value);
+			break;
+
+		case "BEGIN":
+			begin();
+			break;
+
+		case "ROLLBACK":
+			rollback();
+			break;
+
+		case "COMMIT":
+			commit();
+			break;
+
+		case "END":
+			System.exit(0);
+
+		default:
+			System.err.println("Invalid input: " + line + "\n");
+			break;
+
 		}
 	}
-	
-		
-	public void set(String name, int value, boolean isTransaction){		
-		//count(original value) -1
-		if(nameValue.containsKey(name)){
+
+	public void set(String name, int value, boolean isTransaction) {
+		// count(original value) -1
+		if (nameValue.containsKey(name)) {
 			int oldValue = nameValue.get(name);
-			valueCount.put(nameValue.get(name), valueCount.get(nameValue.get(name))-1);
-			//update new name value
+			valueCount.put(nameValue.get(name), valueCount.get(nameValue.get(name)) - 1);
+			// update new name value
 			nameValue.put(name, value);
-			//add to rollbackStack
-			if(isTransaction){
+			// add to rollbackStack
+			if (isTransaction) {
 				String rollbackCommand = "SET " + name + " " + Integer.toString(oldValue);
 				rollbackStack.push(rollbackCommand);
 			}
-		}else{
+		} else {
 			nameValue.put(name, value);
-			if(isTransaction){
+			if (isTransaction) {
 				String rollbackCommand = "UNSET " + name;
 				rollbackStack.push(rollbackCommand);
 			}
 		}
-		
-		//count(new value) + 1
-		if(valueCount.containsKey(value)){
-			valueCount.put(value, valueCount.get(value)+1);
-		}else{
-			valueCount.put(value,1);	
+
+		// count(new value) + 1
+		if (valueCount.containsKey(value)) {
+			valueCount.put(value, valueCount.get(value) + 1);
+		} else {
+			valueCount.put(value, 1);
 		}
-		
+
 	}
-	
-	
-	public void get(String name){
-		if(nameValue.containsKey(name)){
+
+	public void get(String name) {
+		if (nameValue.containsKey(name)) {
 			System.out.println("> " + nameValue.get(name));
-		}else{
+		} else {
 			System.out.println("> " + "NULL");
 		}
-		
+
 	}
-	
-	
-	public void unset(String name, boolean isTransaction){
-		if(nameValue.containsKey(name)){
+
+	public void unset(String name, boolean isTransaction) {
+		if (nameValue.containsKey(name)) {
 			int value = nameValue.get(name);
 			nameValue.remove(name);
-			valueCount.put(value, valueCount.get(value)-1);
-			if(isTransaction){
-				String rollbackCommand = "SET " + name + " "+ Integer.toString(value);
+			valueCount.put(value, valueCount.get(value) - 1);
+			if (isTransaction) {
+				String rollbackCommand = "SET " + name + " " + Integer.toString(value);
 				rollbackStack.push(rollbackCommand);
 			}
 		}
 	}
-		
-	public void numEqualTo(int value){
-		if(valueCount.containsKey(value)){
+
+	public void numEqualTo(int value) {
+		if (valueCount.containsKey(value)) {
 			System.out.println("> " + valueCount.get(value));
-		} else{
+		} else {
 			System.out.println("> " + 0);
 		}
 	}
-	
+
 	public void begin() {
 		numBlocks++;
 		rollbackStack.push("BEGIN");
 	}
-	
+
 	public void rollback() {
-		if(rollbackStack.isEmpty()){
+		if (rollbackStack.isEmpty()) {
 			System.out.println("> " + "NO TRANSACTION");
 			return;
 		}
 		String rollbackCommand = rollbackStack.pop();
-		while(!rollbackCommand.equals("BEGIN")){
+		while (!rollbackCommand.equals("BEGIN")) {
 			String[] tokens = rollbackCommand.split("\\s+");
-			if(tokens[0].equals("SET")){
+			if (tokens[0].equals("SET")) {
 				set(tokens[1], Integer.parseInt(tokens[2]), false);
 			}
-			if(tokens[0].equals("UNSET")){
-				unset(tokens[1],false);
+			if (tokens[0].equals("UNSET")) {
+				unset(tokens[1], false);
 			}
 			rollbackCommand = rollbackStack.pop();
 		}
 		numBlocks--;
 	}
-	
+
 	public void commit() {
-		if(numBlocks==0){
+		if (numBlocks == 0) {
 			System.out.println("> " + "NO TRANSACTION");
 			return;
 		}
--- Solution.java
+++ Solution.java
@@ -10,81 +10,74 @@ import java.util.regex.*;
  * 
  * javac Solution.java
  * 
- * run with command line :
- * java Solution
+ * run with command line : java Solution
  * 
- * run with input file:
- * java Solution inputfilename
+ * run with input file: java Solution inputfilename
  * 
- * Time Complexity:
- * SET/UNSET/GET/NUMEQUALTO/BEGIN/COMMIT O(1) 
- * ROLLBACK O(k), k is the number of commands in a transaction
+ * Time Complexity: SET/UNSET/GET/NUMEQUALTO/BEGIN/COMMIT O(1) ROLLBACK O(k), k
+ * is the number of commands in a transaction
  * 
- * Space Complexity:
- * O(n): use 2 HashMap + 1 stack
+ * Space Complexity: O(n): use 2 HashMap + 1 stack
  */
 
-
 public class Solution {
-	
+
 	public static SimpleDB simpleDB;
-	
+
 	/**
-	 * Read from file input 
+	 * Read from file input
 	 */
 	public static void readFromFile(String fileName) {
-		InputStream is = null; 
-	    InputStreamReader isr = null;
-	    BufferedReader br = null;
-	    
+		InputStream is = null;
+		InputStreamReader isr = null;
+		BufferedReader br = null;
+
 		String line = null;
-		try{
+		try {
 			// open input stream test.txt for reading purpose.
-			is = new FileInputStream(fileName);	         
-	        // create new input stream reader
-			isr = new InputStreamReader(is);	         
-	        // create new buffered reader
+			is = new FileInputStream(fileName);
+			// create new input stream reader
+			isr = new InputStreamReader(is);
+			// create new buffered reader
 			br = new BufferedReader(isr);
-	        
-	        while ((line = br.readLine()) != null) {
-	            System.out.println(line);
-	            simpleDB.handleCommandLine(line);
-	        }       
-	     }catch(IOException e){
-	         e.printStackTrace();
-	     }
+
+			while ((line = br.readLine()) != null) {
+				System.out.println(line);
+				simpleDB.handleCommandLine(line);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
-	
-	
+
 	/**
-	 * Read from user input	  
+	 * Read from user input
 	 * 
 	 */
 	public static void readFromCommandLine() {
 		String line = null;
 		try {
-			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));			
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 			while ((line = br.readLine()) != null) {
-	            System.out.println(line);
-	            simpleDB.handleCommandLine(line);
-	         }  
-			
-		} catch (Exception e) {			
+				System.out.println(line);
+				simpleDB.handleCommandLine(line);
+			}
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
-	
-    public static void main(String args[] ) throws Exception {
-    	simpleDB = new SimpleDB();
-    	
-    	try {
-    		if (args.length == 0)    			
-    		    readFromCommandLine();
-    		else if (args.length == 1)
-    			readFromFile(args[0]);
-    	}catch (Exception e) {
-    		e.printStackTrace();
-    	}    	
-    }
+	public static void main(String args[]) throws Exception {
+		simpleDB = new SimpleDB();
+
+		try {
+			if (args.length == 0)
+				readFromCommandLine();
+			else if (args.length == 1)
+				readFromFile(args[0]);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
